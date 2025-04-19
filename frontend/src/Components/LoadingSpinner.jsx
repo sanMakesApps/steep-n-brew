5e53af003fa73349efb0461585bcb15a85270408
sanMakesApps <thapasanskar017@gmail.com>
2025-04-19 15:02:30 -0400

    feat: add loading state buffer UI, integrate Axios with Zustand for store setup, and implement useUserStore

---
+++ frontend/src/Components/LoadingSpinner.jsx
@@ -0,0 +1,13 @@
+const LoadingSpinner = () => {
+	return (
+		<div className='flex items-center justify-center min-h-screen bg-neutral-100'>
+			<div className='relative'>
+				<div className='w-20 h-20 border-4 border-neutral-300 rounded-full' />
+				<div className='w-20 h-20 border-4 border-t-accent animate-spin rounded-full absolute left-0 top-0' />
+				<span className='sr-only'>Loading</span>
+			</div>
+		</div>
+	);
+};
+
+export default LoadingSpinner;
---
+++ frontend/src/stores/useUserStore.js
@@ -0,0 +1,62 @@
+import { create } from "zustand";
+import axios from "../lib/axios";
+import { toast } from "react-hot-toast";
+
+export const useUserStore = create((set, get) => ({
+	user: null,
+	loading: false,
+	checkingAuth: true,
+
+	signup: async ({ name, email, password, confirmPassword }) => {
+		set({ loading: true });
+
+		if (password !== confirmPassword) {
+			set({ loading: false });
+			return toast.error("Passwords do not match");
+		}
+
+		try {
+			const res = await axios.post("/auth/signup", { name, email, password });
+			set({ user: res.data, loading: false });
+			return toast.success("Account created");
+		} catch (error) {
+			set({ loading: false });
+			toast.error(error.response.data.message || "An error occurred");
+		}
+	},
+	login: async (email, password) => {
+		set({ loading: true });
+
+		try {
+			const res = await axios.post("/auth/login", { email, password });
+			set({ user: res.data, loading: false });
+			toast.success("Welcome back")
+		} catch (error) {
+			set({ loading: false });
+			toast.error(error.response.data.message || "An error occurred");
+		}
+	},
+
+	checkAuth: async () => {
+		set({ checkingAuth: true });
+		try {
+			const response = await axios.get("/auth/profile");
+			set({ user: response.data, checkingAuth: false });
+		} catch (error) {
+			console.log(error.message);
+			set({ checkingAuth: false, user: null });
+		}
+	},
+
+	logout: async () => {
+		try {
+
+			const res = await axios.post('/auth/logout');
+			set({ user: null })
+		} catch (error) {
+			toast.error(error.response.data.message || "An error occurred");
+		}
+	},
+}))
+
+export default useUserStore;
