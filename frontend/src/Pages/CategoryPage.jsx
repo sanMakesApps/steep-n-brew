06204372373acd674b1cc3139e5e0221c9a08bc6
sanMakesApps <thapasanskar017@gmail.com>
2025-04-20 18:00:41 -0400

    feat: Implemented CartPage, CategoryPage, to display FeaturedProducts, LoadingSpinner, Order Summary, PeopleAlsoBought, GiftCouponCard Components.

---
+++ frontend/src/Pages/CategoryPage.jsx
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import { useProductStore } from "../stores/useProductStore";
+import { useParams } from "react-router-dom";
+import { motion } from "framer-motion";
+import ProductCard from "../Components/ProductCard";
+import PageHeaders from "../Components/PageHeaders";
+// import ProductCard from "../components/ProductCard";
+const CategoryPage = () => {
+  const { fetchProductsByCategory, products } = useProductStore();
+
+  const { category } = useParams();
+
+  useEffect(() => {
+    fetchProductsByCategory(category);
+  }, [fetchProductsByCategory, category]);
+
+  console.log("products: ", products);
+  return (
+    <div className="min-h-screen">
+      <div className="relative z-10 max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+        <PageHeaders
+          pageHeader={category.charAt(0).toUpperCase() + category.slice(1)}
+        />
+
+        <motion.div
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 justify-items-center"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+        >
+          {products?.length === 0 && (
+            <h2 className="text-3xl font-semibold text-gray-300 text-center col-span-full">
+              No products found
+            </h2>
+          )}
+
+          {products?.map((product) => (
+            <ProductCard key={product._id} product={product} />
+            // <span>PRODUCT</span>
+          ))}
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPage;
