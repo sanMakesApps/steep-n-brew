c52a6c4ae86fd901a34696d146110c7c40b759ef
sanMakesApps <thapasanskar017@gmail.com>
2025-04-20 18:31:21 -0400

    completed project

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
+import path from "path"
 
 //routes
 import authRoutes from './routes/auth.route.js';
@@ -18,6 +19,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
@@ -30,6 +32,13 @@ app.use('/api/payments', paymentRoutes);
 
 app.use('/api/analytics', analyticsRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log("Server running on http://localhost:", PORT);
