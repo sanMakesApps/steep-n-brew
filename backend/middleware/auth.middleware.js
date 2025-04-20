a36a0cb89cd082d41a410c6c3289a4f09c9b404e
sanMakesApps <thapasanskar017@gmail.com>
2025-04-20 19:37:58 -0400

    backend minor modifications

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,45 +1,41 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
+
 export const protectRoute = async (req, res, next) => {
+	try {
+		const accessToken = req.cookies.accessToken;
 
-    try {
+		if (!accessToken) {
+			return res.status(401).json({ message: "Unauthorized - No access token provided" });
+		}
 
-        const accessToken = req.cookies.accessToken;
+		try {
+			const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+			const user = await User.findById(decoded.userId).select("-password");
 
-        if (!accessToken) {
-            return res.status(401).json({ message: "Token not provided" });
-        }
+			if (!user) {
+				return res.status(401).json({ message: "User not found" });
+			}
 
-        try {
-            const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
-            const user = await User.findById(decoded.userId).select("-password");
+			req.user = user;
 
-            if (!user) {
-                return res.status(401).json({ message: "User not found" });
-            }
-
-            req.user = user;
-            next();
-        } catch (error) {
-            if (error.name === "TokenExpiredError") {
-                return res.status(401).json({ message: "Token expired" });
-            }
-            throw error
-        }
-
-    } catch (error) {
-        console.log('ERROR IN PROTECT ROUTE MIDDLEWARE ', error.message);
-        res.status(401).json({ message: "Unauthorized - Invalid access token" })
-
-    }
-
-}
+			next();
+		} catch (error) {
+			if (error.name === "TokenExpiredError") {
+				return res.status(401).json({ message: "Unauthorized - Access token expired" });
+			}
+			throw error;
+		}
+	} catch (error) {
+		console.log("Error in protectRoute middleware", error.message);
+		return res.status(401).json({ message: "Unauthorized - Invalid access token" });
+	}
+};
 
 export const adminRoute = (req, res, next) => {
-
-    if (req.user && req.user.role === "admin") {
-        next();
-    } else {
-        return res.status(403).json({ message: "Forbidden - Access denied. Admin only." })
-    }
-}
+	if (req.user && req.user.role === "admin") {
+		next();
+	} else {
+		return res.status(403).json({ message: "Access denied - Admin only" });
+	}
+};
